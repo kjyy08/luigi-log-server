a0702e44c695c4dc79701aaca274b54468d94db8
kjyy08 <kjyy08@naver.com>
2025-09-11 18:39:02 +0900

    chore: build.gradle.kts jjwt 라이브러리 변수화

--- libs/common-web/build.gradle.kts
+++ libs/common-web/build.gradle.kts
@@ -10,6 +10,8 @@ dependencyManagement {
     }
 }
 
+val jjwtVersion = "0.12.3"
+
 dependencies {
     implementation(project(":libs:common-domain"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -19,15 +21,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    
-    // JWT
-    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
-    
+
     // Jackson for JSON processing
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:${jjwtVersion}")
+
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:${jjwtVersion}")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:${jjwtVersion}")
+
     // Test dependencies
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
