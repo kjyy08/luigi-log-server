ec388c7d94bb9a98e1762836605b8c0bd1a252f9
kjyy08 <kjyy08@naver.com>
2025-09-27 18:44:25 +0900

    remove: 아키텍처를 벗어나는 코드 제거

--- libs/common-web/src/main/kotlin/cloud/luigi99/blog/common/web/ErrorResponse.kt
+++
@@ -1,9 +0,0 @@
-package cloud.luigi99.blog.common.web
-
-data class ErrorResponse(
-    val error: String,
-    val message: String,
-    val timestamp: Long = System.currentTimeMillis(),
-    val path: String? = null,
-    val details: Map<String, Any>? = null
-)
