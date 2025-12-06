3ec215f9feaa8b8a75866f996f070b34b460efad
kjyy08 <kjyy08@naver.com>
2025-12-07 01:02:41 +0900

    feat: Redis 퍼시스턴스 어댑터 엔티티 및 설정 추가 #11
    - `RedisConfig` 클래스 추가로 Redis 템플릿 및 저장소 설정 정의
    - `RedisDomainEntity` 추상 클래스 구현으로 Redis 엔티티 공통 로직 추가

---
+++ libs/adapter/persistence/redis/src/main/kotlin/cloud/luigi99/blog/adapter/persistence/redis/RedisConfig.kt
@@ -0,0 +1,24 @@
+package cloud.luigi99.blog.adapter.persistence.redis
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+@EnableRedisRepositories(basePackages = ["cloud.luigi99.blog"])
+class RedisConfig {
+    @Bean
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        val template = RedisTemplate<String, Any>()
+        template.connectionFactory = connectionFactory
+        template.keySerializer = StringRedisSerializer()
+        template.valueSerializer = GenericJackson2JsonRedisSerializer()
+        template.hashKeySerializer = StringRedisSerializer()
+        template.hashValueSerializer = GenericJackson2JsonRedisSerializer()
+        return template
+    }
+}
