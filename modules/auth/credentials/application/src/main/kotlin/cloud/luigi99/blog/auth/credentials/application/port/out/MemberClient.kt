f10821597797a068b76235f43967259ea67c4b2c
kjyy08 <kjyy08@naver.com>
2026-01-03 19:27:55 +0900

    refactor: 로그인 유스케이스에 프로필 이미지 URL 필드 추가 #30
    - LoginUseCase: Command 및 Response에 profileImgUrl 필드 추가
    - MemberClient: 회원 가입 요청/응답 DTO에 profileImgUrl 필드 추가
    - LoginService: 로그인 및 회원 가입 로직에서 프로필 이미지 URL 처리

--- modules/auth/credentials/application/src/main/kotlin/cloud/luigi99/blog/auth/credentials/application/port/out/MemberClient.kt
+++ modules/auth/credentials/application/src/main/kotlin/cloud/luigi99/blog/auth/credentials/application/port/out/MemberClient.kt
@@ -3,7 +3,12 @@ package cloud.luigi99.blog.auth.credentials.application.port.out
 interface MemberClient {
     fun execute(request: Request): Response
 
-    data class Request(val email: String, val username: String)
+    data class Request(val email: String, val username: String, val profileImgUrl: String?)
 
-    data class Response(val memberId: String, val email: String, val username: String)
+    data class Response(
+        val memberId: String,
+        val email: String,
+        val username: String,
+        val profileImgUrl: String?,
+    )
 }
