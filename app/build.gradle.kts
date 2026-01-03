c1204e15389c86faf637f05764c28f3702bdd8b1
kjyy08 <kjyy08@naver.com>
2026-01-03 16:55:02 +0900

    chore: 누락된 r2 스토리지 의존성 추가 #57

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation(project(":modules:media:application"))
     implementation(project(":modules:media:adapter:in:web"))
     implementation(project(":modules:media:adapter:out:persistence:jpa"))
+    implementation(project(":modules:media:adapter:out:storage:r2"))
 
     implementation(libs.bundles.spring.boot.web)
     implementation(libs.bundles.spring.boot.data)
