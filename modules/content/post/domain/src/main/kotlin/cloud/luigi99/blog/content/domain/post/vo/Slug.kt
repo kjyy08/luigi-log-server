d3c75d742b537f403e25a659156e8beb102e5495
kjyy08 <kjyy08@naver.com>
2026-01-08 22:24:19 +0900

    chore: 모듈 구조 개선 및 패키지 경로 변경
    - content 모듈을 세분화하여 post 서브모듈로 리팩토링
    - 관련 패키지 경로를 content.post로 이동
    - build.gradle.kts 파일의 의존성 경로 수정
    - settings.gradle.kts 의 모듈 경로 반영
    
    BREAKING CHANGE: content 관련 기존 경로가 변경되어 참조 코드 수정 필요

---
+++ modules/content/post/domain/src/main/kotlin/cloud/luigi99/blog/content/domain/post/vo/Slug.kt
@@ -0,0 +1,34 @@
+﻿package cloud.luigi99.blog.content.post.domain.post.vo
+
+import cloud.luigi99.blog.common.domain.ValueObject
+
+/**
+ * Post의 URL slug
+ *
+ * URL에 사용되는 고유 식별자로 한글, 영문 소문자, 숫자, 하이픈, 대괄호를 허용합니다.
+ * - 한글(가-힣), 영문 소문자(a-z), 대문자(A-Z), 숫자(0-9), 하이픈(-), 대괄호([])만 사용 가능
+ * - 하이픈으로 시작하거나 끝날 수 없음
+ * - 연속된 하이픈 불가
+ * - 500자 이하
+ *
+ * @property value slug 문자열
+ * @throws IllegalArgumentException slug 형식이 잘못된 경우
+ */
+@JvmInline
+value class Slug(val value: String) : ValueObject {
+    init {
+        require(value.isNotBlank()) { "Slug cannot be blank" }
+        require(value.length <= 500) {
+            "Slug must not exceed 500 characters (actual: ${value.length})"
+        }
+        require(value.matches(SLUG_REGEX)) {
+            "Invalid slug format: $value (allowed: Korean(가-힣), letters(a-zA-Z), numbers, hyphens, and brackets)"
+        }
+    }
+
+    companion object {
+        private val SLUG_REGEX = "^[a-zA-Z0-9가-힣\\[\\]]+(-[a-zA-Z0-9가-힣\\[\\]]+)*$".toRegex()
+    }
+
+    override fun toString(): String = value
+}
