e27898a1c91f7ae46685942f2da767dfc7609a65
kjyy08 <kjyy08@naver.com>
2026-01-09 21:24:34 +0900

    chore: CI 워크플로 및 Dockerfile 최적화
    - CI 워크플로에서 `fetch-depth` 옵션 제거 및 테스트 작업 이름 변경 ('Run tests and build'로 변경)
    - JAR 아티팩트 업로드 작업 및 Docker 빌드 단계에서 아티팩트 다운로드 단계 추가
    - Dockerfile에서 멀티스테이지 빌드를 단순화하고 빌드 이미지 제거
    - `build.gradle.kts`의 그룹 및 설명 수정
    - `gradle.properties` 파일 추가로 병렬 빌드 및 버전 관리 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-group = "cloud.luigi99"
-version = "0.0.1-SNAPSHOT"
-description = "Luigi Blog Server"
-
 allprojects {
+    group = "cloud.luigi99"
+    description = "Luigi Log Server"
+
     repositories {
         mavenCentral()
     }
