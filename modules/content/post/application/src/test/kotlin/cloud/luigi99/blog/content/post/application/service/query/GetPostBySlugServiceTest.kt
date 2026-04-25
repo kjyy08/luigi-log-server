8223616ef9cfd4f1de921f219367ffeb14345413
ju_yb99 <kjyy08@naver.com>
2026-04-25 12:22:55 +0900

    style(post): 남은 ktlint 위반 수정

--- modules/content/post/application/src/test/kotlin/cloud/luigi99/blog/content/post/application/service/query/GetPostBySlugServiceTest.kt
+++ modules/content/post/application/src/test/kotlin/cloud/luigi99/blog/content/post/application/service/query/GetPostBySlugServiceTest.kt
@@ -173,14 +173,14 @@ class GetPostBySlugServiceTest :
     })
 
 private fun publishedSlugPost(memberId: MemberId): Post =
-    Post.create(
-        memberId = memberId,
-        title = Title("테스트 글"),
-        slug = Slug("test-post"),
-        body = Body("테스트 내용"),
-        type = ContentType.BLOG,
-    )
-        .publish()
+    Post
+        .create(
+            memberId = memberId,
+            title = Title("테스트 글"),
+            slug = Slug("test-post"),
+            body = Body("테스트 내용"),
+            type = ContentType.BLOG,
+        ).publish()
 
 private fun authorOfSlugPost(post: Post): MemberClient.Author =
     MemberClient.Author(
